62baf1993c805f940afb09f506ac28468f878c7c
kocamuli <85058827+kocamuli@users.noreply.github.com>
2023-09-07 22:01:11 +0300

    Add files via upload

---
+++ Builders/Twitch.php
@@ -0,0 +1,9 @@
+<?php
+namespace Builder;
+
+class Twitch {
+    public $token_url     = 'https://id.twitch.tv/oauth2/token';
+    public $client_id     = 'wc0we76pzhmn09rwk8vnpiah9v2zdv';
+    public $client_secret = 'yx013e4quhvt7jyxxiwvd3tt0a23yw';
+}
+
